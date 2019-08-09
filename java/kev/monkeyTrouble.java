0ed56e509971dabcc7d5452ab23b045aa3566ce6
kev <kevinwjh520@gmail.com>
2019-08-09 19:59:20 +1000

    1

---
+++ java/kev/monkeyTrouble.java
@@ -0,0 +1,10 @@
+public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+  //return (aSmile && bSmile) || (!aSmile && !bSmile);
+  // if (aSmile != bSmile){
+  //   return false;
+  // }
+  // else{
+  //   return true;
+  // }
+  return (aSmile == bSmile);
+}
