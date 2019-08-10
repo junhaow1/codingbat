faff23a9a0f51935d8dfeced4185b3aee7752960
kev <kevinwjh520@gmail.com>
2019-08-10 23:24:38 +1000

    String

---
+++ java/kev/helloName.java
@@ -0,0 +1,10 @@
+public String helloName(String name) {
+  //System.out.println("Hello");
+  return "Hello" + " "+name +"!" ;
+}
+
+// 空格在上一个string里打一个空格就好了
+
+// public String helloName(String name) {
+//   return "Hello " + name + "!";
+// }
