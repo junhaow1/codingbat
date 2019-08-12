1cfeef2859be221386a6f5332b841f32ab19ed0c
kev <kevinwjh520@gmail.com>
2019-08-12 20:36:01 +1000

    string questions done on monday ezclap

---
+++ java/kev/extraEnd.java
@@ -0,0 +1,6 @@
+public String extraEnd(String str) {
+  //return str.substring(Math.max(str.length() - 2, 0));
+  return str.substring(str.length()-2)+str.substring(str.length()-2)+str.substring(str.length()-2);
+
+
+}
