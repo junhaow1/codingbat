f4d4aacc048715d4df3034cfdd7c398fc79cfc2f
kev <kevinwjh520@gmail.com>
2019-08-19 16:50:26 +1000

    fix stupid question

--- java/kev/theEnd.java
+++ java/kev/theEnd.java
@@ -1,11 +1,23 @@
 public String theEnd(String str, boolean front) {
-  if (front) {
-    return str.substring(0,1);
-  }
-  if (!front){
-    return str.substring(str.length()-1,str.length());
-  }
-  else{
-    return str;
-  }
+//   if (front) {
+//     return str.substring(0,1);
+//   }
+//   if (!front){
+//     return str.substring(str.length()-1,str.length());
+//   }
+//   else{
+//     return str;
+//   }
+// }
+
+
+//!!! if else 不用蠢！！！
+
+
+    if (front){
+        return str.substring(0,1);
+    }   else {
+        return str.substring(str.length()-1,str.length());
+    }
+
 }
