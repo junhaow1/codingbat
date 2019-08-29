b831b51646dec82518b634b9a39373d2099b966d
kev <kevinwjh520@gmail.com>
2019-08-29 13:47:03 +1000

    tut02 q01 upload

--- java/kev/conCat.java
+++ java/kev/conCat.java
@@ -6,10 +6,15 @@ public String conCat(String a, String b) {
   // if (b.equals("") ){
   //   return a;
   // }
-  if (a.substring( a.length()-1,a.length() )).equals( b.substring(0,1) ) {
+  if ( a.substring( a.length()-1,a.length() )).equals( b.substring(0,1) ) {
     return "1";
     //a + b.substring(1,b.length())
   }
   // return a.substring(a.length()-1,a.length()) ;
   return a+b;
+}
+
+
+if ((a.substring(a.length()-1, a.length())).equals(b.substring(0,1))) {
+
 }
