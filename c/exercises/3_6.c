70f65a59bbc7ac4a93a6fc4fe60cde75def78fa0
kev <kevinwjh520@gmail.com>
2020-01-15 01:35:05 +1100

    周二去体检 晚上回家 补到3.5h 把第三章3、道题做完了 第四章开始了 看了这章的四分之一

---
+++ c/exercises/3_6.c
@@ -0,0 +1,67 @@
+//
+//  main.c
+//  3.60
+//
+//  Created by Junhao Wang on 11/1/20.
+//  Copyright © 2020 Junhao Wang. All rights reserved.
+//
+
+#include <stdio.h>
+#include <limits.h>
+
+#define MAXLINE 1000
+
+void itoa(int n, char s[], int w);
+void reverse(char s[]);
+
+int main(void)
+{
+    char s[MAXLINE];
+    int width;
+
+    width = 11;
+    itoa(INT_MIN, s, width);
+    printf("%12d is converted to %s.\n", INT_MIN, s);
+    itoa(826, s, width);
+    printf("%12d is converted to %s.\n", 826, s);
+    itoa(-2093, s, width);
+    printf("%12d is converted to %s.\n", -2093, s);
+
+    return 0;
+}
+
+void itoa(int n, char s[], int w)
+{
+    int i, sign, remainder, k, j;
+
+    sign = n;
+    i = 0;
+    do {
+        remainder = n % 10;
+        s[i++] = ((sign < 0) ? -remainder : remainder) + '0';
+    } while (n /= 10);
+    if (sign < 0)
+        s[i++] = '-';
+    
+    while (i < w)
+        s[i++] = ' ';
+
+    s[i] = '\0';
+    reverse(s);
+}
+
+void reverse(char s[])
+{
+    int i, j;
+    int tmp;
+
+    for (j = 0; s[j] != '\0'; ++j)
+        ;
+    --j;
+
+    for (i = 0; i < j; ++i, --j) {
+        tmp = s[i];
+        s[i] = s[j];
+        s[j] = tmp;
+    }
+}
