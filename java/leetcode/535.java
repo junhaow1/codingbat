e9bb7bbdabfc6c7a79af87e1be29c7117c49bc46
Junhao Wang <junhaow1@student.unimelb.edu.au>
2020-09-02 15:51:11 +1000

    wednesday medium

---
+++ java/leetcode/535.java
@@ -0,0 +1,28 @@
+public class Codec {
+
+    Map<String,String> my = new HashMap<String,String>();
+    String base = "http://tinyurl.com/";
+
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        // String base = "http://tinyurl.com/";
+        String short1 = base + longUrl.hashCode();
+        my.put(short1,longUrl);
+        return short1;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return my.get(shortUrl);
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
+
+
+//medium question
+//https://leetcode.com/problems/encode-and-decode-tinyurl/
+//535. Encode and Decode TinyURL
