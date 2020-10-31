cc99924a2b3794f16f2cc0ac24306c434bac155f
Junhao Wang <junhaow1@student.unimelb.edu.au>
2020-10-31 23:46:29 +1100

    周六

---
+++ java/leetcode/213.java
@@ -0,0 +1,22 @@
+class NumArray {
+
+    private int[] sums;
+
+    public NumArray(int[] nums) {
+        sums = new int[nums.length + 1];
+        for (int i = 1; i <= nums.length; i++) {
+            sums[i] = sums[i - 1] + nums[i - 1];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sums[j + 1] - sums[i];
+    }
+}
+
+//  dynamic programming
+//求区间 i ~ j 的和，可以转换为 sum[j + 1] - sum[i]，其中 sum[i] 为 0 ~ i - 1 的和。
+
+
+//https://leetcode.com/problems/range-sum-query-immutable/
+// 303. Range Sum Query - Immutable
--- java/leetcode/303.java
+++ java/leetcode/303.java
@@ -1,22 +1,28 @@
-class NumArray {
-
-    private int[] sums;
-
-    public NumArray(int[] nums) {
-        sums = new int[nums.length + 1];
-        for (int i = 1; i <= nums.length; i++) {
-            sums[i] = sums[i - 1] + nums[i - 1];
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
         }
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        return Math.max(rob(nums, 0, n - 2), rob(nums, 1, n - 1));
     }
 
-    public int sumRange(int i, int j) {
-        return sums[j + 1] - sums[i];
+    private int rob(int[] nums, int first, int last) {
+        int pre2 = 0, pre1 = 0;
+        for (int i = first; i <= last; i++) {
+            int cur = Math.max(pre1, pre2 + nums[i]);
+            pre2 = pre1;
+            pre1 = cur;
+        }
+        return pre1;
     }
 }
 
-//  dynamic programming
-//求区间 i ~ j 的和，可以转换为 sum[j + 1] - sum[i]，其中 sum[i] 为 0 ~ i - 1 的和。
 
 
-//https://leetcode.com/problems/range-sum-query-immutable/
-// 303. Range Sum Query - Immutable
+
+//https://leetcode.com/problems/house-robber-ii/
+// 213. House Robber II
